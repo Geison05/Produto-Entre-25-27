61ef59c715e5406593dc0bf90c01eec91916d52a
Geison Amorim <79721330+Geison05@users.noreply.github.com>
2022-04-09 19:41:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner (System.in);
+    //Variavel
+    int primeiroNumero = 25;
+    int segundoNumero = 27;
+    int produto = 0;
+    //Processamento
+    produto = primeiroNumero * segundoNumero;  
+    //Saida
+    System.out.println("Produto :" + produto);
+  }
+}
